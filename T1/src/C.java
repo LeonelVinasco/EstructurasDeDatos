dbb34b352fc911a1855588a79d6cce1a67e11713
Leonel Vinasco <leonelvinasco@gmail.com>
2016-08-22 13:31:00 -0500

    Aún no funciona C, mirar stringBuilder. Falta ordenar posibles resultados, y contar los que son mayores a la entrada

--- T1/src/C.java
+++ T1/src/C.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class C {
+static int cantidad=0;
+static StringBuilder builder = new StringBuilder();
 
 	public static void main(String[] args) throws FileNotFoundException {
 
@@ -14,47 +16,53 @@ public class C {
 		}else{
 			scan = new Scanner(System.in);
 		}
+		/*String str_casos[];
+		str_casos=scan.nextLine().split(" ");
+		int casos=Integer.parseInt(str_casos[0]);/*/
+		
+		String entrada = null;
+		char[] char_letras;
+		char letraFija='a';
+		
 		String str_casos[];
 		str_casos=scan.nextLine().split(" ");
 		int casos=Integer.parseInt(str_casos[0]);
-		String str_letras;
-		char[] char_letras;
-		char letraFija='a';
-		for(int contCasos=0;contCasos<casos;contCasos++){
-			str_letras=scan.nextLine();
-			char_letras=str_letras.toCharArray();
-		    
-			int nAct=char_letras.length;
-			
 		
-			//String elementosOrdenados = builder.toString();
-			for( int lugarLetraFija=0; lugarLetraFija<char_letras.length-1;lugarLetraFija++){
-				letraFija=char_letras[lugarLetraFija];
-				for(int letraDos=0; letraDos<char_letras.length-1;letraDos++){
-					if (lugarLetraFija==letraDos)
-						continue;
-					
-				}
-				
-				
-				
+		for(int contCasos=0;contCasos<casos;contCasos++){
+			cantidad=0;
+			//str_letras="a,b,c,d,e".split(",");
+			entrada=scan.nextLine();
+			char_letras=entrada.toCharArray();
+			String str_letras[]=new String[char_letras.length];
+			for(int i=0;i<char_letras.length;i++){
+				str_letras[i]=Character.toString(char_letras[i]);	
 			}
 			
+			int r=str_letras.length;
 			
+			for(String d : str_letras) 
+			System.out.println(d);
+			
+			System.out.println(str_letras.length-1);
+			
+			permutacion(str_letras,"",r,r);	
+				
+			System.out.println(builder.toString());	
+			}
 		
-		}
-
 	}
 	
-	public static void permutacion(String[] elem, String act, int n, int r){
+	private static void permutacion(String[] elem, String act, int n, int r){
 		if(n==0){
 			System.out.println(act);
-			
+			cantidad++;
+			 
+				builder.append(act);
 		}else{
 			for(int i=0;i<r;i++){
-				if (!act.contains(elem[i])){
-					permutacion(elem, act+elem[i]+", ", n-1,r);
-				}
+				//if (!act.contains(elem[i])){
+					permutacion(elem, act+elem[i]+",", n-1,r);
+				//}
 				
 				
 			}
