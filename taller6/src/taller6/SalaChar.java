7b2556792610f83f5ce0a7761c1da6a5148164eb
debian <debian@localhost>
2017-06-02 01:08:44 -0500

    Funcionando el problema de sala de chat UN. El problema que tenía y por el cual ningun caso pasaba , es porque no estaba inicializando los hash ni las variables cuando empezaba un nuevo caso, es decir todos los datos quedaban almacenados para el suiente ciclo de for.

--- taller6/src/taller6/SalaChar.java
+++ taller6/src/taller6/SalaChar.java
@@ -2,6 +2,8 @@ package taller6;
 
 import java.util.*;
 
+import java.util.*;
+
 public class SalaChar {
 
 	public static void main(String[] args) {
@@ -11,7 +13,13 @@ public class SalaChar {
 		String in;
 		String entrada;
 		
-		int temp;
+	
+		
+		
+			boolean numeric=true;
+		while(scanner.hasNext()){
+			//System.out.print("\n");
+				int temp;
 		String tempStringNum;
 		int tempNum;//number that we found on string
 		int casos;
@@ -20,15 +28,9 @@ public class SalaChar {
 		TreeMap<String, Integer> tablaAux= new TreeMap<String, Integer>();
 		int lugarN=0;
 		String realAlias;
-		boolean numeric=false;
+	
 		int start=0;
-String out="sd";
-		
-		
-		
-		while(scanner.hasNext()){
-			//System.out.print("\n");
-			
+       
 			in=scanner.next();
 			entrada=in;
 			casos=Integer.parseInt(entrada);
@@ -40,15 +42,15 @@ String out="sd";
 				//find if there is a numeric value on the input
 				char[] l=entrada.toCharArray();
 				
-				for(int j=0;j<entrada.length();j++){
+				/*for(int j=0;j<entrada.length();j++){
 					
 					if(l[j]=='0'||l[j]=='1'||l[j]=='2'||l[j]=='3'||l[j]=='4'||l[j]=='5'||l[j]=='6'||
 							l[j]=='7'||l[j]=='8'||l[j]=='9'){
 						lugarN=j;
 						numeric=true;
 					}
-				}
-				if (numeric){
+				}*/
+				/*if (numeric){
 					numeric=false;
 					tempStringNum=entrada.substring(lugarN,entrada.length());
 					entrada=entrada.substring(0,lugarN);
@@ -70,11 +72,10 @@ String out="sd";
 						System.out.print("\n");
 					}
 					
-				}else{
+				}else{*/
 					if (tabla.containsKey(entrada)){
 						temp=tabla.get(entrada);
-						tabla.remove(entrada);
-						tabla.put(entrada, temp+1);
+						tabla.replace(entrada, temp+1);
 						tabla.put(entrada.concat(Integer.toString(temp+1)), temp+1);
 						System.out.print(entrada.concat(Integer.toString(temp+1)));
 						System.out.print("\n");
@@ -86,7 +87,7 @@ String out="sd";
 						
 					}
 					
-				}
+				
 				
 				
 				
